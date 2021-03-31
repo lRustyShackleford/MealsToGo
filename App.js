1359df326a682e4f120d645dfe93290d06a45e12
doctorshoe <linares.damian@gmail.com>
2021-03-30 22:04:39 -0700

    added restaurant files

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Searchbar } from "react-native-paper";
-import {
-  StatusBar,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-} from "react-native";
-
-const isAndroid = Platform.OS === "android";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen"
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const RestaurantInfo = () => <Text>Restaurant Info</Text>
+
