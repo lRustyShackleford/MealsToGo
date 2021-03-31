674f64e0f79c5cbdda96908d28f68fdae2433cbc
doctorshoe <linares.damian@gmail.com>
2021-03-31 12:40:59 -0700

    fixed styling

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,18 +7,18 @@ import styled from "styled-components/native";
 
 const SafeView = styled(SafeAreaView)`
   flex: 1;
-  marginTop: StatusBar.currentHeight%;
-`
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
 const SearchContainer = styled(View)`
-  padding: 16px;
-`
+  padding: ${(props) => props.theme.space[3]};
+`;
 
 const RestaurantListContainer = styled(View)`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
-`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
 
 export const RestaurantsScreen = () => (
   <SafeView>
