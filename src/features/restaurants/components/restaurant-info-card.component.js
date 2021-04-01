8b2639f4a4ebbb5224b506d40036c19d7a651428
doctorshoe <linares.damian@gmail.com>
2021-03-31 18:11:23 -0700

    minor changes

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   ${(props) => props.theme.colors.bg.primary};
@@ -28,38 +29,66 @@ const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
 const Rating = styled(View)`
   flex-direction: row;
   padding-top: ${(props) => props.theme.space[2]}
   padding-bottom: ${(props) => props.theme.space[2]}
 `;
 
+const ImageStyled = styled(Image)`
+  margin-left: 10;
+  width: 20px;
+  height: 20px;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://4.bp.blogspot.com/-lDiaVQQFlVw/Xqi2HiN-F7I/AAAAAAABS5Y/xCUQqqnp6dk2bhR_mY-HSmP5cEG3vvlLQCNcBGAsYHQ/w1200-h630-p-k-no-nu/krabby-patty-spongebob-squarepants-nickelodeon-nick-sbsp-animated-gif.gif",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
-  console.log(ratingArray);
+
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-        {ratingArray.map(() => (
-        <SvgXml xml={star} width={20} height={20} />
-        ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            {isOpenNow && <SvgXml xml={open} width={20} height={20}></SvgXml>}
+            <ImageStyled source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
